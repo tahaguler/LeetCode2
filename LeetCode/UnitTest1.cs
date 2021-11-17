43af39e59f27ba4f5930fde7e494f3b629e7f922
tahaguler <tahaguler95@gmail.com>
2021-11-16 22:59:24 -0500

    updated the test

--- LeetCode/UnitTest1.cs
+++ LeetCode/UnitTest1.cs
@@ -105,13 +105,7 @@ namespace LeetCode
                 int complement = target - nums[i];
                 if (hashTable.ContainsKey(complement))
                 {
-                    var result = new int[] {i, (int) hashTable[complement]};
-                    foreach (var t in result)
-                    {
-                        _testOutputHelper.WriteLine(t.ToString());
-                    }
-
-                    return result;
+                    return new int[] {i, (int) hashTable[complement]};
                 }
 
                 hashTable.Add(nums[i], i);
@@ -143,13 +137,13 @@ namespace LeetCode
         }
 
         [Fact]
-        public void LongestSubStreing()
+        public void MaxOperator()
         {
             Math.Max(5, 6);
         }
 
         [Fact]
-        public void CanIAddSameCharIntoHashSet()
+        public void CanIAddSameCharIntoHashSet() // I can't. it wont crash but it only keeps one.
         {
             HashSet<char> set = new HashSet<char>();
             set.Add('a');
@@ -183,6 +177,111 @@ namespace LeetCode
             }
 
             _testOutputHelper.WriteLine(ans.ToString());
+        }
+
+        [Fact]
+        public ListNode AddTwoNumbers()
+        {
+            ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+            ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+
+            ListNode dummyHead = new ListNode();
+            ListNode p = l1, q = l2, curr = dummyHead; // I need a dummy head to keep track of the start, otherwise I would lose the start. 
+            var carry = 0;
+
+            while (p != null || q != null)
+            {
+                var x = (p != null) ? p.val : 0;
+                var y = (q != null) ? q.val : 0;
+
+                int sum = x + y + carry;
+
+                carry = sum / 10;
+
+                curr.next = new ListNode( sum % 10); // 0 -> x   modulus operator called mod.
+                curr = curr.next; // x -> null
+                
+                if( p != null) p = p.next;
+                if (q != null) q = q.next;
+            }
+            
+            if (carry > 0){
+                curr.next = new ListNode(carry);
+            }
+
+            return dummyHead.next;
+        }
+
+        [Fact]
+        public string LongestPali()
+        {
+            string s = "babad";
+            if (s == null || s.Length < 1) return "";
+           
+            int start = 0, end = 0, realLen = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                int len1 = paliLength(s, i, i);
+                int len2 = paliLength(s, i, i + 1);
+                int len = Math.Max(len1, len2);
+                if (len > realLen)
+                {
+                    start = i - (len - 1) / 2;
+                    realLen = len;
+                }
+            } 
+
+            return s.Substring(start, realLen);
+        }
+
+        [Fact]
+        public int CountPrimes()
+        {
+            int n = 10;
+            
+            
+            bool[] primes = new bool[n];
+        
+            for(int i = 0; i < primes.Length; i++){
+                primes[i] = true;
+            }
+        
+            for(int i = 2; i * i < primes.Length; i++){
+                if(primes[i]){
+                    for(int j = i; i * j < primes.Length; j++){
+                        primes[i*j] = false;
+                    }
+                }
+            }
+        
+            int primeCount = 0;
+            for(int i = 2; i < primes.Length; i++){
+                if(primes[i]) primeCount++;
+            }
+        
+            return primeCount;
+        }
+        
+        private int paliLength(string s, int left, int right){
+            int L = left, R = right;
+            while(L >= 0 && R < s.Length && s[L] == s[R]){
+                L--;
+                R++; 
+            }
+            return R - L - 1 ;// rwcecdb 3 2 4 lentgh 3 
+        } 
+    }
+
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
         }
     }
 }
